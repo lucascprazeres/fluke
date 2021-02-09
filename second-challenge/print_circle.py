21722360ee2c93e63295abaf0931f571ed1ccea3
Lucas Prazeres <olucascostadp@gmail.com>
2021-02-09 16:56:14 -0300

    challenge2 - removed useless lines

--- second-challenge/print_circle.py
+++ second-challenge/print_circle.py
@@ -1,29 +1,23 @@
-import math
-
-
 def print_circle_with_radius_of(radius):
     if type(radius) is not int or radius < 1 or radius > 100:
-        print('Invalid entry - radius must be an integer between 1 and 100')
+        print("argument 'radius' must be an integer between 1 and 100.")
         return
 
     diameter = radius * 2
     jump = 2
-    print(jump)
     # TOP
     for value in range(radius, diameter, jump):
-        left_indent = ' ' * (diameter-value)
+        indentation = ' ' * (diameter-value)
         symbols = '* ' * value
-        print(left_indent + symbols)
+        print(indentation + symbols)
     # MIDDLE
     for value in range(0, radius):
-        left_indent = ' '
-        symbols = '* ' * diameter
-        print(symbols)
+        print('* ' * diameter)
     # BOTTOM
     for value in range(diameter, radius, -jump):
-        left_indent = ' ' * (diameter-value)
+        indentation = ' ' * (diameter-value)
         symbols = '* ' * value
-        print(left_indent + symbols)
+        print(indentation + symbols)
 
 
 print_circle_with_radius_of(5)
