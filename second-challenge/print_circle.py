46a423744a2a9d508346f823a21e5dbde9969283
Lucas Prazeres <olucascostadp@gmail.com>
2021-02-09 16:16:45 -0300

    challenge2 - implemented the loops responsible for the circle printing

--- second-challenge/print_circle.py
+++ second-challenge/print_circle.py
@@ -1,17 +1,29 @@
+import math
+
+
 def print_circle_with_radius_of(radius):
     if type(radius) is not int or radius < 1 or radius > 100:
         print('Invalid entry - radius must be an integer between 1 and 100')
         return
 
-    # FIRST HALF OF THE CIRCLE
-    # loop though a range of values until reach the radius of the circle
-    # calculate the number of symbols to print
-    # create a row made of blank spaces
-    # place the symbols in the middle of the row
-    # print the result
-
-    # SECOND HALF OF THE CIRCLE
-    #do the same, looping from the radius to the first value
+    diameter = radius * 2
+    jump = 2
+    print(jump)
+    # TOP
+    for value in range(radius, diameter, jump):
+        left_indent = ' ' * (diameter-value)
+        symbols = '* ' * value
+        print(left_indent + symbols)
+    # MIDDLE
+    for value in range(0, radius):
+        left_indent = ' '
+        symbols = '* ' * diameter
+        print(symbols)
+    # BOTTOM
+    for value in range(diameter, radius, -jump):
+        left_indent = ' ' * (diameter-value)
+        symbols = '* ' * value
+        print(left_indent + symbols)
 
 
 print_circle_with_radius_of(5)
