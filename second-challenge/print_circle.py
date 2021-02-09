7af31e2c6632ab8f0479e328d9ba23a7949b20d4
Lucas Prazeres <olucascostadp@gmail.com>
2021-02-09 20:10:53 -0300

    challenge2 - refactored the printing loops

--- second-challenge/print_circle.py
+++ second-challenge/print_circle.py
@@ -3,21 +3,20 @@ def print_circle_with_radius_of(radius):
         print("argument 'radius' must be an integer between 1 and 100.")
         return
 
-    diameter = radius * 2
-    jump = 2
-    # TOP
-    for value in range(radius, diameter, jump):
-        indentation = ' ' * (diameter-value)
-        symbols = '* ' * value
-        print(indentation + symbols)
-    # MIDDLE
-    for value in range(0, radius):
-        print('* ' * diameter)
-    # BOTTOM
-    for value in range(diameter, radius, -jump):
-        indentation = ' ' * (diameter-value)
-        symbols = '* ' * value
-        print(indentation + symbols)
+    diameter = 2 * radius
+    print('\n')
+
+    for y in range(1, diameter+1):
+        for x in range(1, diameter+1):
+            pos_y = (y-radius)**2
+            pos_x = (x-radius)**2
+            if abs(pos_x + pos_y) < radius**2:
+                print('* ', end="")
+            else:
+                if x == diameter:
+                    print('')
+                else:
+                    print('  ', end="")
 
 
 print_circle_with_radius_of(5)
