38ffe344dfb12591508337d96c0ffaae22b731c5
Lucas Prazeres <olucascostadp@gmail.com>
2021-02-10 15:19:45 -0300

    api - implemeted the registerNewCostumer route

--- fluke-api/src/routes.ts
+++ fluke-api/src/routes.ts
@@ -1,6 +1,38 @@
 import { Router } from 'express';
+import { hash } from 'bcryptjs';
+import dbclient from './database/connection';
 
 const routes = Router();
+
+routes.post('/registerNewCostumer', async (request, response) => {
+  const { name, email, CPF, phonenumber, password } = request.body;
+
+  let queryResult;
+
+  try {
+    await dbclient.connect();
+
+    const encryptedPassword = await hash(password, 8);
+
+    queryResult = await dbclient.db().collection('customers').insertOne({
+      name,
+      email,
+      CPF,
+      phonenumber,
+      password: encryptedPassword,
+    });
+
+    await dbclient.close();
+  } catch (err) {
+    console.log(err);
+  }
+
+  const customer = queryResult?.ops[0];
+
+  delete customer.password;
+
+  return response.json(customer);
+});
 
 routes.get('/pipipi', (request, response) => {
   return response.json({ message: 'popopo' });
