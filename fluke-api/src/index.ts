10fdb7818aa73a073540a6399299fbf2ca636e37
Lucas Prazeres <olucascostadp@gmail.com>
2021-02-10 22:43:59 -0300

    api - added validation on the registerNewCustomer route

--- fluke-api/src/index.ts
+++ fluke-api/src/index.ts
@@ -1,6 +1,9 @@
+import 'reflect-metadata';
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { errors as celebrateModuleErrors } from 'celebrate';
+import 'express-async-errors';
 import routes from './routes';
 
 const app = express();
@@ -8,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(celebrateModuleErrors());
 
 const port = 3333;
 
--- fluke-api/src/routes.ts
+++ fluke-api/src/routes.ts
@@ -1,10 +1,26 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import CustomerController from './controllers/CustomerController';
 
 const routes = Router();
 const customerController = new CustomerController();
 
-routes.post('/registerNewCostumer', customerController.create);
+const createCustomerValidationSchema = {
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    CPF: Joi.string().required(),
+    phonenumber: Joi.string().required(),
+    password: Joi.string().required(),
+  },
+};
+
+routes.post(
+  '/registerNewCostumer',
+  celebrate(createCustomerValidationSchema, { abortEarly: false }),
+  customerController.create,
+);
 
 routes.get('/pipipi', (request, response) => {
   return response.json({ message: 'popopo' });
