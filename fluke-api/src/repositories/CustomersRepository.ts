3c3dc4fc6ee30cfbd3cdd5ade7c327b0daacfa19
Lucas Prazeres <olucascostadp@gmail.com>
2021-02-10 22:16:12 -0300

    api - put the two functions on customersRapository together again and fix wrong CPF storing

--- fluke-api/src/repositories/CustomersRepository.ts
+++ fluke-api/src/repositories/CustomersRepository.ts
@@ -3,44 +3,35 @@ import dbclient from '../database/connection';
 import { CreateCustomer, Customer } from '../interfaces';
 
 export default class CustomersRepository {
-  // Responsible for error handling
   async create(data: CreateCustomer): Promise<Customer> {
     let customer;
     try {
-      customer = await this.createCustomer(data);
+      await dbclient.connect();
+
+      const encryptedPassword = await hash(data.password, 8);
+
+      const queryResult = await dbclient
+        .db()
+        .collection('customers')
+        .insertOne({
+          name: data.name,
+          email: data.email,
+          CPF: data.CPF,
+          phonenumber: data.phonenumber,
+          password: encryptedPassword,
+          availablePackets: {
+            gb: 0,
+            minutes: 0,
+          },
+          orderedPackets: [],
+        });
+
+      await dbclient.close();
+
+      [customer] = queryResult.ops;
     } catch (err) {
       console.log(err.message);
-      // try again
-      customer = await this.createCustomer(data);
     }
-
-    return customer;
-  }
-
-  private async createCustomer(data: CreateCustomer): Promise<Customer> {
-    await dbclient.connect();
-
-    const encryptedPassword = await hash(data.password, 8);
-
-    const queryResult = await dbclient
-      .db()
-      .collection('customers')
-      .insertOne({
-        name: data.name,
-        email: data.email,
-        CPF: data.password,
-        phonenumber: data.phonenumber,
-        password: encryptedPassword,
-        availablePackets: {
-          gb: 0,
-          minutes: 0,
-        },
-        orderedPackets: [],
-      });
-
-    await dbclient.close();
-
-    const customer = queryResult.ops[0];
 
     return customer;
   }
